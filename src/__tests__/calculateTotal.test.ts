aaaec6e9d963ebc4d7df1cdf7ffc8e9534c49729
StijnVM <stijn.vm@hotmail.com>
2025-06-19 16:33:41 +0200

    bugfix

--- src/__tests__/calculateTotal.test.ts
+++ src/__tests__/calculateTotal.test.ts
@@ -9,6 +9,10 @@ test("lune -2 & soleil double terre & terre +2", () => {
 	const values = { lune: 1, soleil: 2, terre: 1 };
 	expect(calculateTotal(values)).toBe(5);
 });
+test("lune /2 & soleil no terre", () => {
+	const values = { lune: 2, soleil: 1, terre: 2 };
+	expect(calculateTotal(values)).toBe(1);
+});
 test("lune /2 & soleil double terre & terre +2", () => {
 	const values = { lune: 2, soleil: 2, terre: 1 };
 	expect(calculateTotal(values)).toBe(4);
--- src/utils/calculateTotal.ts
+++ src/utils/calculateTotal.ts
@@ -6,9 +6,12 @@ export const calculateTotal = (values: Values): number => {
 	let total: number = lune + soleil + terre;
 
 	if (lune === 1) total -= 2;
-	if (soleil === 1) total -= terre;
-	if (soleil === 2) total += terre;
+	if (soleil === 1) {
+		total -= terre;
+		if (lune === 2) total = Math.floor(total / 2);
+	}
 	if (soleil === 2) {
+		total += terre;
 		if (terre === 1) total += 2;
 		if (lune === 2) total = Math.floor(total / 2);
 		if (terre === 2) total = 6;
